94488a5ecb4ef0d085fed2a9ec7597dd52c82ecd
DMX330 <emilliechrissie6@gmail.com>
2025-12-19 09:06:30 +0300

    Add files via upload

---
+++ design.js
@@ -0,0 +1,10 @@
+
+    function setColor(color) {
+        document.body.style.backgroundColor = color;
+    }
+
+    function randomColor() {
+        let colors = ["red", "green", "blue"];
+        let randomIndex = Math.floor(Math.random() * colors.length);
+        document.body.style.backgroundColor = colors[randomIndex];
+    }
